b54fa7e6b4f378fa7f626e53e6acf5836d88b610
Sheldon Kwok <me@sheldonk.com>
2025-12-23 11:52:00 -0800

    feat: Add file name input parser

--- 2025/utils.ts
+++ 2025/utils.ts
@@ -1,8 +1,14 @@
+import { basename } from 'node:path';
+
 const EXAMPLE_SEPARATOR = "----";
 
 const IS_REAL = Bun.argv.includes("-r");
+const entryNum = Number(basename(Bun.main).split('.')[0]);
 
-export async function getInput(num: number): Promise<string> {
+export async function getInput(num: number = entryNum): Promise<string> {
+
+  if (isNaN(num)) throw new Error(`Invalid input number ${num}`)
+
   const file = await Bun.file(`${num}.input`);
   const text = await file.text();
 
